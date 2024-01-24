5d31acc8fc79f972ec52c78d8488e3c1df1c745b
Client Steven Frilles <stvnfrlls@gmail.com>
2024-01-24 12:18:30 +0800

    update show.blade.php

--- resources/views/concerns/show.blade.php
+++ resources/views/concerns/show.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
     <div class="container mx-auto m-3 p-3">
-
         <div class="h1 text-start mb-3">Details</div>
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card mb-2">
                     <div class="card-body">
                         <div class="d-flex justify-content-between">
-                            <div class="fw-bold">{{ $concern->name }}</div>
+                            <div class="fw-bold">{{ $concern->name }} ({{ $concern->email }})</div>
                             <div>{{ $concern->created_at->format('Y-m-d') }}</div>
                         </div>
                     </div>
@@ -20,10 +19,4 @@
             </div>
         </div>
     </div>
-
-    <script>
-        $(document).ready(function() {
-
-        });
-    </script>
 @endsection
